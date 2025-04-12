add0d926f0bd772df9b96329fcce9e940112133a
Kiriki122 <kiriki496@gmail.com>
2025-04-12 13:20:51 +0300

    На страницу профиля добавлен слайдер с товарами пользователя.

--- Frontend/src/pages/profile-page/ui/profile-product-slider/index.tsx
+++ Frontend/src/pages/profile-page/ui/profile-product-slider/index.tsx
@@ -1,11 +1,15 @@
 import { useUserStore } from '@/entities/user/model';
-import style from './style.module.scss';
 import { useQuery } from '@tanstack/react-query';
 import { getProductListByUserId } from '@/entities/product/model/api';
 import { ProfileProductCard } from '@/entities/product/ui';
 import { isAxiosError } from 'axios';
 import { Link } from 'react-router-dom';
 import { getViewItemPath } from '@/shared/config/routes';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Virtual } from 'swiper/modules';
+import 'swiper/swiper-bundle.css';
+
+import style from './style.module.scss';
 
 export const ProfileProductSlider = () => {
     const { id } = useUserStore((state) => state.user);
@@ -28,7 +32,7 @@ export const ProfileProductSlider = () => {
             return (
                 <div className={`${style.profile__products} ${style.products}`}>
                     Ошибка соединения с сервером. Код ошибки:
-                    {` ${error.code} ${error.message}`}
+                    {` ${error.response?.status} ${error.message}`}
                 </div>
             );
         }
@@ -39,12 +43,46 @@ export const ProfileProductSlider = () => {
         <div className={`${style.profile__products} ${style.products}`}>
             <h2 className={style.products__header}>Активные лоты</h2>
             <ul className={style.products__list}>
-                {data.length > 0 ? (
-                    data.map((item) => (
-                        <Link to={getViewItemPath(item.id)} key={item.id}>
-                            <ProfileProductCard product={item} />{' '}
-                        </Link>
-                    ))
+                {data?.length > 0 ? (
+                    <Swiper
+                        grabCursor
+                        centeredSlides
+                        spaceBetween={50}
+                        slidesPerView={1}
+                        modules={[Virtual]}
+                        breakpoints={{
+                            500: {
+                                slidesPerView: 2,
+                            },
+                            651: {
+                                slidesPerView: 3,
+                            },
+                            769: {
+                                slidesPerView: 1,
+                            },
+                            1100: {
+                                slidesPerView: 2,
+                            },
+                            1400: {
+                                slidesPerView: 3,
+                            },
+                            1800: {
+                                slidesPerView: 4,
+                            },
+                        }}
+                    >
+                        {data.map((item) => (
+                            <SwiperSlide>
+                                <Link
+                                    to={getViewItemPath(item.id)}
+                                    key={item.id}
+                                    className={style.products__link}
+                                >
+                                    <ProfileProductCard product={item} />{' '}
+                                </Link>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 ) : (
                     <div>Вы еще не выставили товары.</div>
                 )}
