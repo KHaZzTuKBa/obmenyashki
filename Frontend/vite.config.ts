0dcddcfff65da32190aa1c4112baee5db1fa2d63
Kiriki122 <kiriki496@gmail.com>
2025-04-13 23:57:04 +0300

    Добавил Dev profile в Docker

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,4 +6,23 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths(), svgr()],
+    server: {
+        host: '0.0.0.0',
+        port: 80,
+        hmr: {
+            clientPort: 80,
+        },
+        watch: {
+            usePolling: true,
+        },
+        proxy: {
+            '/api': {
+                target: 'http://backend:80',
+                changeOrigin: true,
+
+                //   Опционально: для поддержки проксирования WebSocket (например, для SignalR)
+                ws: true,
+            },
+        },
+    },
 });
