4b35300dbb2b42268711c3c6273fa359015faf95
Kiriki122 <kiriki496@gmail.com>
2025-04-14 06:12:07 +0300

    Улуччшение внешнего вида

--- Frontend/src/features/header/search/ui/SearchForm.tsx
+++ Frontend/src/features/header/search/ui/SearchForm.tsx
@@ -1,4 +1,4 @@
-import { useSearchForm } from '@/features/header/search/model/useSearchForm'; // Импорт хука
+import { useSearchForm } from '../hooks/useSearchForm';
 import { Icon } from '@/shared/ui/Icon';
 
 import styles from './style.module.scss';
@@ -7,22 +7,25 @@ export const SearchForm = () => {
     const { query, handleQueryChange, handleSubmit } = useSearchForm();
     return (
         <form className={styles.search} onSubmit={handleSubmit} role='search'>
-            {/* <label className={styles.search} htmlFor="main-search"> */}
-            {/* Скрываем текст лейбла визуально, но оставляем для скринридеров */}
-            {/* <span className={styles.visuallyHidden}>Поиск по товарам</span> */}
-            <span className={styles.search__glass}>
-                <Icon name='search' />
-            </span>
-            <input
-                className={styles.search__input}
-                id='main-search'
-                type='search'
-                name='q'
-                placeholder='Поиск'
-                value={query}
-                onChange={handleQueryChange}
-            />
-            {/* </label> */}
+            <label
+                className={`${styles.search} ${styles.search__label}`}
+                htmlFor='main-search'
+            >
+                {/* Скрываем текст лейбла визуально, но оставляем для скринридеров */}
+                {/* <span className={styles.visuallyHidden}>Поиск по товарам</span> */}
+                <span className={styles.search__glass}>
+                    <Icon name='search' />
+                </span>
+                <input
+                    className={styles.search__input}
+                    id='main-search'
+                    type='search'
+                    name='q'
+                    placeholder='Поиск'
+                    value={query}
+                    onChange={handleQueryChange}
+                />
+            </label>
             {/* Можно добавить скрытую кнопку submit для лучшей доступности */}
             {/* <button type="submit" className={styles.visuallyHidden}>Найти</button> */}
         </form>
