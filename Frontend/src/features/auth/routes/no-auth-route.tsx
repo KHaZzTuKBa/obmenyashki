78d08ca1d554fca051912cd7fc277496e496226a
Kiriki122 <kiriki496@gmail.com>
2025-04-19 01:58:21 +0300

    Fix fix'а

--- Frontend/src/features/auth/routes/no-auth-route.tsx
+++ Frontend/src/features/auth/routes/no-auth-route.tsx
@@ -1,21 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { isAuth } from '@/entities/user/api/api';
 
-const isAuthorized = async (): Promise<boolean> => {
-    const response = await isAuth();
-    return response;
-};
 
-export const NoAuthRoute = async ({
+
+export const NoAuthRoute =  ({
     children,
     redirectTo = '/',
 }: {
     children: ReactNode;
     redirectTo: string;
 }) => {
-    if (await isAuthorized()) {
+    const { data, isPending, isError } = useQuery({
+        queryKey: ['auth-check'],
+        queryFn: isAuth,
+        staleTime: 5 * 60 * 1000, 
+        retry: 1,
+    });
+
+    if (isPending) {
+        return <div>Загрузка...</div>;
+    }
+
+    if ( isError || !data) {
         return <Navigate to={redirectTo} replace />;
     }
 
--- Frontend/src/features/auth/routes/protected-route.tsx
+++ Frontend/src/features/auth/routes/protected-route.tsx
@@ -1,19 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import { ReactNode } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 
 import { isAuth } from '@/entities/user/api/api';
 import { Path } from '@/shared/config/routes';
 
-const isAuthorized = async (): Promise<boolean> => {
-    const response = await isAuth();
-    return response;
-};
-
-export const ProtectedRoute = async ({ children }: { children: ReactNode }) => {
+export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
     const location = useLocation();
 
-    if (!(await isAuthorized())) {
+    const { data, isPending, isError } = useQuery({
+        queryKey: ['auth-check'],
+        queryFn: isAuth,
+        staleTime: 5 * 60 * 1000,
+        retry: 1,
+    });
+
+    if (isPending) {
+        return <div>Загрузка...</div>;
+    }
+
+    if (isError || !data) {
         return <Navigate to={Path.LOGIN} replace state={{ from: location }} />;
     }
+
     return <>{children}</>;
 };
