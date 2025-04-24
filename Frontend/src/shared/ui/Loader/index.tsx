626426588d346bf3f450fb9427828a9ede1b4a63
Kiriki122 <kiriki496@gmail.com>
2025-04-25 00:54:59 +0300

    Fix: значение по умолчанию для className в Loader

--- Frontend/src/shared/ui/Loader/index.tsx
+++ Frontend/src/shared/ui/Loader/index.tsx
@@ -6,7 +6,7 @@ type LoaderProps = React.ComponentProps<typeof BounceLoader> & {
     wrapperClassName?: string;
 };
 
-export const Loader = ({ wrapperClassName, ...props }: LoaderProps) => {
+export const Loader = ({ wrapperClassName = '', ...props }: LoaderProps) => {
     return (
         <div className={`${style.loader__wrapper} ${wrapperClassName}`}>
             <BounceLoader color='#4DC5A5' size='6em' {...props} />
